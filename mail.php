598723d81aac04ff69c337b1fdbbbb06fdbf7b87
Dmitry <jokkercdv@gmail.com>
2023-05-10 17:31:43 +0300

    form

--- mail.php
+++ mail.php
@@ -1,79 +1,50 @@
 <?php
 
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exeption;
-//use PHPMailer\PHPMailer\SMTP;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exeption;
+
+
 
 
 require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/SMTP.php';
 require 'phpmailer/src/Exception.php';
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$email = $_POST['email'];
-$text = $_POST['message'];
-$phone = $_FILES['phone'];
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHtml(true);
 
-// Формирование самого письма
-$title = "Заголовок письма";
-$body = "
-<h2>Новое письмо</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br>
-<b>Почта:</b> $email<br><br>
-<b>Сообщение:</b><br>$text
-";
+$mail->setFrom('service@intel-trans.ru', 'Форма сайта');
+$mail->addAddress('info@intel-trans.ru');
+$mail->Subject = 'Письмо с сайта';
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-try {
-    $mail->isSMTP();
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
-    //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-    // Настройки вашей почты
-    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-    $mail->Username   = 'rudinsky.mitya'; // Логин на почте
-    $mail->Password   = 'gjboezudinjnhesu'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('rudinsky.mitya@yandex.ru', 'Имя отправителя'); // Адрес самой почты и имя отправителя
-
-    // Получатель письма
-    $mail->addAddress('ivan5wot5@yandex.ru');
-    //$mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
-
-    // Прикрипление файлов к письму
-//if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-        $filename = $file['name'][$ct];
-        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-            $rfile[] = "Файл $filename прикреплён";
-        } else {
-            $rfile[] = "Не удалось прикрепить файл $filename";
-        }
-    }
-//}
-// Отправка сообщения
-$mail->isHTML(true);
-$mail->Subject = $title;
-$mail->Body = $body;
-
-// Проверяем отравленность сообщения
-if ($mail->send()) {$result = "success";}
-else {$result = "error";}
-
-} catch (Exception $e) {
-    $result = "error";
-    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+$body = '<h1>Письмо с сайта</h1>';
+if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['phone']))){
+    $body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['message']))){
+    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
 }
 
-// Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+$mail->Body = $body;
 
+
+
+if (!$mail->send()) {
+    $message = 'Ошибка';
+} else {
+    $message = 'Данные отправлены';
+}
+
+$response = ['message' => $message];
+header('Content-type: application/json');
+echo json_encode($response);
 ?>
